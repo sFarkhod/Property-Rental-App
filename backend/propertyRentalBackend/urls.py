60522937da97f183a833ca63d970a38987c463fb
sFarkhod <sohibovfarhod03@gmail.com>
2023-11-20 17:47:13 +0500

    requirements and swagger documentation added

--- backend/propertyRentalBackend/urls.py
+++ backend/propertyRentalBackend/urls.py
@@ -16,9 +16,28 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.urls import re_path
+from rest_framework import permissions
+from drf_yasg.views import get_schema_view
+from drf_yasg import openapi
 
 from propertyRentalBackend.views import sign_up, login, logout, create_real_estate, update_user, password_reset_request, \
     password_reset_confirm, list_real_estates, get_real_estate, update_real_estate, delete_real_estate
+
+
+schema_view = get_schema_view(
+   openapi.Info(
+      title="Snippets API",
+      default_version='v1',
+      description="Test description",
+      terms_of_service="https://www.google.com/policies/terms/",
+      contact=openapi.Contact(email="contact@snippets.local"),
+      license=openapi.License(name="BSD License"),
+   ),
+   public=True,
+   permission_classes=(permissions.AllowAny,),
+)
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -36,4 +55,8 @@ urlpatterns = [
     path('real-estate/<int:pk>/', get_real_estate, name='real-estate-detail'),
     path('real-estates/<int:real_estate_id>/update/', update_real_estate, name='update_real_estate'),
     path('real-estates/<int:real_estate_id>/delete/', delete_real_estate, name='delete_real_estate'),
+
+    path('swagger<format>/', schema_view.without_ui(cache_timeout=0), name='schema-json'),
+    path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
+    path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'),
 ]
